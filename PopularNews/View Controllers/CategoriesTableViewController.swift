0c0f68a2ccafd2a1de78ac2e4b1385f0858760bd
Alexander Sergeev <sergeev_ul73@mail.ru>
2021-12-15 14:35:34 +0400

    Initial Commit

---
+++ PopularNews/View Controllers/CategoriesTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CategoriesTableViewController.swift
+//  PopularNews
+//
+//  Created by Сергеев Александр on 15.12.2021.
+//
+
+import UIKit
+
+class CategoriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+}
