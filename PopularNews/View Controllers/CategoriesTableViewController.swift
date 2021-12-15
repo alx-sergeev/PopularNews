405a22a700138a4606fc8bda5cb87ef7ad02bb96
Alexander Sergeev <sergeev_ul73@mail.ru>
2021-12-15 15:13:29 +0400

    Реализовал вывод категорий

--- PopularNews/View Controllers/CategoriesTableViewController.swift
+++ PopularNews/View Controllers/CategoriesTableViewController.swift
@@ -9,20 +9,39 @@ import UIKit
 
 class CategoriesTableViewController: UITableViewController {
 
+    // MARK: - Properties
+    let categoryCell = "categoryCell"
+    let categories = News.getCategories()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+}
 
+extension CategoriesTableViewController {
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categories.count
     }
     
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: categoryCell, for: indexPath)
+        var cellConfiguration = cell.defaultContentConfiguration()
+        
+        cellConfiguration.text = categories[indexPath.row]
+        
+        cell.contentConfiguration = cellConfiguration
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
