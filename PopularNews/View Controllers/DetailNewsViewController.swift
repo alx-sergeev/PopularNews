ba0b7a37e90bdb3fb28ff3a849bb690eb5bb5f22
Alexander Sergeev <sergeev_ul73@mail.ru>
2021-12-20 14:15:26 +0400

    Верстка детальной страницы новости

---
+++ PopularNews/View Controllers/DetailNewsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailNewsViewController.swift
+//  PopularNews
+//
+//  Created by Сергеев Александр on 20.12.2021.
+//
+
+import UIKit
+
+class DetailNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
